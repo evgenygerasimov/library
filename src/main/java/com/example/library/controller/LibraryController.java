397dd6a7afd3e8d90df58dfd33a29b8204cb1bd4
evgenygerasimov <evgenygerasimoff@gmail.com>
2024-11-14 23:37:55 +0400

    refactoring code

--- src/main/java/com/example/library/controller/LibraryController.java
+++ src/main/java/com/example/library/controller/LibraryController.java
@@ -1,16 +1,12 @@
 package com.example.library.controller;
 
 import com.example.library.entity.Author;
-import com.example.library.entity.Employee;
 import com.example.library.entity.Reader;
 import com.example.library.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
